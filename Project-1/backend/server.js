06262501cd26be62512b4e5b1be4cabff9d59604
this-is-yogesh <ayogesh1214@gmail.com>
2023-12-26 14:10:34 +0530

    cors and proxy

--- Project-1/backend/server.js
+++ Project-1/backend/server.js
@@ -3,10 +3,16 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 
-app.get("/",(req,res)=>{
-    res.send("welcome to home")
-})
+app.get("/api/data", (req, res) => {
+  const arrayofdata = [
+    { id: 1, title: "Title-1", Content: "Content-1" },
+    { id: 2, title: "Title-2", Content: "Content-2" },
+    { id: 3, title: "Title-3", Content: "Content-3" },
+  ];
 
-app.listen(PORT, ()=>{
-    console.log("welcome to the server")
-})
+  res.send(arrayofdata);
+});
+
+app.listen(PORT, () => {
+  console.log(`welcome to the server http://localhost:${PORT}/`);
+});
--- Project-1/frontend/src/App.jsx
+++ Project-1/frontend/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("/api/data")
+  //     .then((response) => {
+  //       if (response.ok) {
+  //         return response.json();
+  //       }
+  //       throw new Error("Something went wrong");
+  //     })
+  //     .then((responseJson) => {
+  //       console.log(responseJson, "fetch response");
+  //       //setData(responseJson)
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // });
+
+  useEffect(() => {
+    axios
+      .get("/api/data")
+      .then((res) => setData(res.data))
+      .catch((e) => console.log(e, "error"));
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {data.map((d) => (
+        <div key={d.id}>
+          <p>{d.id}</p>
+          <p>{d.title}</p>
+          <p>{d.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
